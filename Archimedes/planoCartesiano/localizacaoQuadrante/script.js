5e3d9b977295a3b7a9b16a9e8eac47fc139e4565
Leonardo Silva Beling <leoytprivado@gmail.com>
2026-04-13 17:07:53 -0300

    Atividade de plano cartesiano archimedes

---
+++ Archimedes/planoCartesiano/localizacaoQuadrante/script.js
@@ -0,0 +1,41 @@
+// PASSO 1: Referênciar as variáveis
+const inAbscissa = document.getElementById("inGolsA");
+const inOrdenada = document.getElementById("inGolsB");
+const btResult = document.getElementById("btResult");
+const outResult = document.getElementById("outResult");
+
+// PASSO 2: Definir Função
+function calcular () {
+    // Variáveis de controle
+    var resultado = ""
+    var abscissa = inAbscissa.value;
+    var ordenada = inOrdenada.value;
+    if (abscissa == "" || ordenada == "") {
+        alert("Parece qua algum campo não foi preenchido")
+    }
+    else {
+        if (abscissa >= 0 && ordenada >= 0){
+            resultado = "O ponto informado, localiza-se no 1° Quadrante do plano cartesiano."
+        }
+        else if (abscissa <= 0 && ordenada >= 0){
+            resultado = "O ponto informado, localiza-se no 2° Quadrante do plano cartesiano."
+        }
+        else if (abscissa <= 0 && ordenada <= 0){
+            resultado = "O ponto informado, localiza-se no 3° Quadrante do plano cartesiano."
+        }
+        else if (abscissa >= 0 && ordenada <= 0){
+            resultado = "O ponto informado, localiza-se no 4° Quadrante do plano cartesiano."
+        }
+        else if (abscissa != 0 && ordenada == 0){
+            resultado = "O ponto informado está sob o eixo X do plano cartesiano."
+        }
+        else if (abscissa == 0 && ordenada != 0){
+            resultado = "O ponto informado está sob o eixo Y do plano cartesiano."
+        }
+        else if (abscissa == 0 && ordenada == 0){
+            resultado = "O PONTO INFORMADO ESTÁ NA ORIGEM (CENTRO) DO PLANO CARTESIANO."
+        }
+    }
+}
+
+// PASSO 3: Associar função à escuta de evento
