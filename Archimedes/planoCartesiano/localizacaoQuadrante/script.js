d537ecb9683c324001b5062cd44ff2a3a812eb20
Leonardo Silva Beling <leoytprivado@gmail.com>
2026-04-13 17:18:21 -0300

    Ajustes necessários para bom funcionamento do código - archimedes

--- Archimedes/planoCartesiano/localizacaoQuadrante/script.js
+++ Archimedes/planoCartesiano/localizacaoQuadrante/script.js
@@ -1,6 +1,6 @@
 // PASSO 1: Referênciar as variáveis
-const inAbscissa = document.getElementById("inGolsA");
-const inOrdenada = document.getElementById("inGolsB");
+const inAbscissa = document.getElementById("inAbscissa");
+const inOrdenada = document.getElementById("inOrdenada");
 const btResult = document.getElementById("btResult");
 const outResult = document.getElementById("outResult");
 
@@ -11,31 +11,33 @@ function calcular () {
     var abscissa = inAbscissa.value;
     var ordenada = inOrdenada.value;
     if (abscissa == "" || ordenada == "") {
-        alert("Parece qua algum campo não foi preenchido")
+        alert("Parece qua algum campo não foi preenchido");
     }
     else {
-        if (abscissa >= 0 && ordenada >= 0){
-            resultado = "O ponto informado, localiza-se no 1° Quadrante do plano cartesiano."
-        }
-        else if (abscissa <= 0 && ordenada >= 0){
-            resultado = "O ponto informado, localiza-se no 2° Quadrante do plano cartesiano."
-        }
-        else if (abscissa <= 0 && ordenada <= 0){
-            resultado = "O ponto informado, localiza-se no 3° Quadrante do plano cartesiano."
-        }
-        else if (abscissa >= 0 && ordenada <= 0){
-            resultado = "O ponto informado, localiza-se no 4° Quadrante do plano cartesiano."
+        if (abscissa == 0 && ordenada == 0){
+            resultado = "O PONTO INFORMADO ESTÁ NA ORIGEM (CENTRO) DO PLANO CARTESIANO.";
         }
         else if (abscissa != 0 && ordenada == 0){
-            resultado = "O ponto informado está sob o eixo X do plano cartesiano."
+            resultado = "O ponto informado está sob o eixo X do plano cartesiano.";
         }
         else if (abscissa == 0 && ordenada != 0){
-            resultado = "O ponto informado está sob o eixo Y do plano cartesiano."
+            resultado = "O ponto informado está sob o eixo Y do plano cartesiano.";
         }
-        else if (abscissa == 0 && ordenada == 0){
-            resultado = "O PONTO INFORMADO ESTÁ NA ORIGEM (CENTRO) DO PLANO CARTESIANO."
+        else if (abscissa >= 0 && ordenada >= 0){
+            resultado = "O ponto informado, localiza-se no 1° Quadrante do plano cartesiano.";
+        }
+        else if (abscissa <= 0 && ordenada >= 0){
+            resultado = "O ponto informado, localiza-se no 2° Quadrante do plano cartesiano.";
+        }
+        else if (abscissa <= 0 && ordenada <= 0){
+            resultado = "O ponto informado, localiza-se no 3° Quadrante do plano cartesiano.";
+        }
+        else if (abscissa >= 0 && ordenada <= 0){
+            resultado = "O ponto informado, localiza-se no 4° Quadrante do plano cartesiano.";
         }
     }
+    outResult.textContent = resultado;
 }
 
 // PASSO 3: Associar função à escuta de evento
+btResult.addEventListener("click", calcular)
