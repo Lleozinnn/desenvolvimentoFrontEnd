540a1244f5c6569bfb74153830f75ac973854d1b
Leonardo Silva Beling <20261TSI0032@cefetes.br>
2026-05-06 09:26:40 -0300

    Continuação das notas academicas

--- Archimedes/notaAcademica/script.js
+++ Archimedes/notaAcademica/script.js
@@ -14,4 +14,23 @@ btVerificar.addEventListener("click", verificarAluno);
 // Criação da função para verificar aluno
 function verificarAluno () {
     // Verifica os campos de entrada
+    let nota1 = Number(inNota1.value)
+    let nota2 = Number(inNota2.value)
+    let faltas = Number(inFaltas.value)
+    if (inNota1.value == "" && nota1 >= 0) {
+        alert("Informe a primeira nota")
+        inNota1.focus()
+    }
+    else if (inNota2.value == "" && nota2 >= 0) {
+        alert("Informe a segunda nota")
+        inNota2.focus()
+    }
+    else if (inFaltas.value == "" && faltas >= 0) {
+        alert("Informe a quantidade de faltas")
+        inFaltas.focus()
+    }
+    else {
+        // Com so campos preenchidos, verifica-se o aluno.
+        
+    }
 }
